a9471b57f549fbfc2f5b298fde9b67418638fd66
cyrine05 <srairisyrine@harguia.r-iset.tn>
2022-10-19 21:19:46 +0100

    sign in

---
+++ lib/signin.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SignInState();
+  }
+}
+
+class SignInState extends State<SignIn> {
+  String _name="";
+  String _familyName="";
+  String _password="";
+  String _phoneNumber="";
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildName() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Name'),
+      maxLength: 10,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Name is Required';
+        }
+
+        return null;
+      },
+      onSaved: (value) {
+        _name = value!;
+      },
+    );
+  }
+
+  Widget _buildFamilyName() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Family Name'),
+      maxLength: 10,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Family Name is Required';
+        }
+
+        return null;
+      },
+      onSaved: (value) {
+        _name = value!;
+      },
+    );
+  }
+
+  Widget _buildPassword() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Password'),
+      keyboardType: TextInputType.visiblePassword,
+      validator: ( value) {
+        if (value!.isEmpty) {
+          return 'Password is Required';
+        }
+
+        return null;
+      },
+      onSaved: (value) {
+        _password = value!;
+      },
+    );
+  }
+
+  Widget _buildPhoneNumber() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Phone number'),
+      keyboardType: TextInputType.phone,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Phone number is Required';
+        }
+        return null;
+      },
+      onSaved: ( value) {
+        _phoneNumber = value!;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreenAccent.shade700,
+        toolbarHeight: 30,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(50),
+                bottomLeft: Radius.circular(50))),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildName(),
+                _buildFamilyName(),
+                _buildPassword(),
+                _buildPhoneNumber(),
+                SizedBox(height: 100),
+                RaisedButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(80.0)),
+                  padding: EdgeInsets.all(0.0),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Color(0xff4ABB47), Color(0xff97D136)],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                        ),
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: Container(
+                      constraints:
+                          BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        "SIGN IN",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
